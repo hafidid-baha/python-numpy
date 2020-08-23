add61a702de4088e2d7d199f48bd8a383d145042
hafid id-baha <hafidsoftdev@gmail.com>
2020-08-23 16:45:15 +0100

    getting type of numpy array

--- app.py
+++ app.py
@@ -8,5 +8,14 @@ twodimarray = np.array([[0,1,2],[4,5,6]])
 print(twodimarray)
 
 # getting the number of array dimentions
+print("number of dimentions")
 print(array.ndim)
 print(twodimarray.ndim)
+#getting the number of columns and rows
+print("the shape of array")
+print(array.shape)
+print(twodimarray.shape)
+#getting the type of array
+print("array type")
+print(array.dtype)
+print(twodimarray.dtype)
