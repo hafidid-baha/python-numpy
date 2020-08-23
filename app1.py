f9db2e4e87aa4698b4970f225217c759bec9ea15
hafid id-baha <hafidsoftdev@gmail.com>
2020-08-23 17:09:58 +0100

    getting specific element from numpy array

--- app1.py
+++ app1.py
@@ -3,3 +3,6 @@ import numpy as np
 array = np.array([[1,2,3,4,5,6],[7,8,9,10,11,12]])
 # getting the array shape (rows,columns)
 print(array.shape)
+
+# getting a specific element [row,column]
+print(array[0,5]) #prints 6
