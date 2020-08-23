3cccb98e6e8b7753a6faa2f70a2bdd2c8608ff14
hafid id-baha <hafidsoftdev@gmail.com>
2020-08-23 18:12:40 +0100

    using full_like funtion too clone shape of existing array

--- app2.py
+++ app2.py
@@ -3,12 +3,15 @@ import numpy as np
 # initlize array in numpy module
 # create 0s matrix
 array =  np.zeros((2,2)) #create two dim array 2 rows and 2 cols
-print(array)
+# print(array)
 
 #create 0s matrix
 array2 = np.ones((3,2))
-print(array2)
+# print(array2)
 
 #create and init array with specific number
 array3 = np.full((2,3,4),4)
-print(array3)
+# print(array3)
+#using full like function to clone the shape of existing array
+array4 = np.full_like(array3,7)
+print(array4)
